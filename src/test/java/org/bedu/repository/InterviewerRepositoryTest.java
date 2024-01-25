6942e40830553235e54d04ebff9c200a75de3229
Jorge Ramón <jorge.ramon2128@gmail.com>
2024-01-24 19:10:25 -0600

    Updated tests

--- src/test/java/org/bedu/repository/InterviewerRepositoryTest.java
+++ src/test/java/org/bedu/repository/InterviewerRepositoryTest.java
@@ -8,10 +8,13 @@ import java.util.List;
 import org.bedu.model.Interviewer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @DataJpaTest
 public class InterviewerRepositoryTest {
   
